1c7bd8cb523ce4ce54bf95f57c00a199c064ea86
Evan Quach <evanq126@vt.edu>
2020-12-13 17:26:06 -0500

    fixed comments

--- src/c_utils/zset.c
+++ src/c_utils/zset.c
@@ -5,16 +5,9 @@
  * in order to get O(log(N)) INSERT and REMOVE operations into a sorted
  * data structure.
  *
- * The elements are added to a hash table mapping Redis objects to scores.
+ * The elements are added to a hash table mapping uids to scores.
  * At the same time the elements are added to a skip list mapping scores
- * to Redis objects (so objects are sorted by scores in this "view").
- *
- * Note that the SDS string representing the element is the same in both
- * the hash table and skiplist in order to save memory. What we do in order
- * to manage the shared SDS string more easily is to free the SDS string
- * only in zslFreeNode(). The dictionary has no value free method set.
- * So we should always remove an element from the dictionary, and later from
- * the skiplist.
+ * to uids (so uids are sorted by scores in this "view").
  *
  * This skiplist implementation is almost a C translation of the original
  * algorithm described by William Pugh in "Skip Lists: A Probabilistic
@@ -23,11 +16,7 @@
  * b) the comparison is not just by key (our 'score') but by satellite data.
  * c) there is a back pointer, so it's a doubly linked list with the back
  * pointers being only at "level 1". This allows to traverse the list
- * from tail to head, useful for ZREVRANGE. */
-
-
-/* For our case, we do not need to do c) and for a) we can point to list
- * for duplicate scores */
+ * from tail to head, useful for reverse ranges in the future. */
 
 #include "zset.h"
 
